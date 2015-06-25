3db83c0fce2bd01e995e1bb48436f90174af43a1
apatil1 <apatil1@jhu.edu>
2015-06-25 14:38:39 -0400

    fixed taking inputs

--- phyloprofilerlib/main.py
+++ phyloprofilerlib/main.py
@@ -16,21 +16,21 @@ def run_command(command, error_message):
 
 def get_args(argv):
     parser = argparse.ArgumentParser(description="Runs Metaphlan2.")
-    parser.add_argument("--R1", required=True,
+    parser.add_argument("--forward-reads", required=True,
                         type=argparse.FileType("r"),
                         help="R1.fastq")
-    parser.add_argument("--R2", required=True,
+    parser.add_argument("--reverse-reads", required=True,
                         type=argparse.FileType("r"),
                         help="R2.fastq")
-    parser.add_argument("--output", required=True,
+    parser.add_argument("--output-file", required=True,
                         type=argparse.FileType("w"),
                         help="output file")
     args = parser.parse_args(argv)
-    check_file_exists_or_die(args.R1.name)
-    check_file_exists_or_die(args.R2.name)
-    args.R1.close()
-    args.R2.close()
-    args.output.close()
+    check_file_exists_or_die(args.forward-reads.name)
+    check_file_exists_or_die(args.reverse-reads.name)
+    args.forward-reads.close()
+    args.reverse-reads.close()
+    args.output-file.close()
     return(args)
 
 def run_metaphlan(R1, R2, output):
@@ -44,7 +44,7 @@ def run_metaphlan(R1, R2, output):
 
 def main(argv=None):
     args = get_args(argv)
-    run_metaphlan(args.R1.name, args.R2.name, args.output.name)
+    run_metaphlan(args.forward-reads.name, args.reverse-reads.name, args.output-file.name)
 
 if __name__=="__main__":
     main()
