0e970066e5e4d6b06a3f85e37edbc146f49fb0a3
Kyle Bittinger <kylebittinger@gmail.com>
2015-07-01 14:55:10 -0400

    Fixed the existing test.

--- test/test_main.py
+++ test/test_main.py
@@ -1,17 +1,33 @@
 import unittest
-import metaphlan_wrapper
 import os
+
+from phyloprofilerlib.main import Metaphlan
+
 
 class MetaphlanWrapperTest(unittest.TestCase):
     def setUp(self):
+        self.config = {
+            "metaphlan_fp": "metaphlan2.py",
+            "mpa_pkl": "pa_v20_m200.pkl",
+            "bowtie2db": "mpa_v20_m200",
+        }
         self.r1 = "fake_genome1-R1.fastq"
         self.r2 = "fake_genome1-R2.fastq"
         self.out = "out"
-       
-  
+
     def test_main(self):
-        metaphlan_wrapper.run_metaphlan(self.r1, self.r2, self.out)
-        self.assertTrue(os.path.isfile(self.out + ".txt"))
+        app = Metaphlan(self.config)
+        observed = app.make_command(self.r1, self.r2)
+        expected = [
+            'python', 'metaphlan2.py',
+            'fake_genome1-R1.fastq,fake_genome1-R2.fastq',
+            '--mpa_pkl', 'pa_v20_m200.pkl',
+            '--bowtie2db', 'mpa_v20_m200',
+            '--nproc', '5',
+            '--bowtie2out', 'out.bowtie2.bz2',
+            '--input_type', 'fastq',
+        ]
+        self.assertEqual(observed, expected)
 
 if __name__=="__main__":
     unittest.main()
